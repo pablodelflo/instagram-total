84b70928faee5f3df761b62b92ef5caf08074f42
Pablo Delgado <pabloj.df@gmail.com>
2026-04-14 15:00:03 +0200

    Añadido driver manager

--- config-sample.py
+++ config-sample.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 BASE_PATH_COLECCIONES = Path("C:/ruta/donde-guardar/colecciones")
 DEBUG_PORT = "127.0.0.1:9222"
-PROFILE_INSTAGRAM = "https://www.instagram.com/username/"  # Reemplaza con el perfil de Instagram que deseas analizar
+PROFILE_INSTAGRAM = "https://www.instagram.com/username/"  # Reemplaza con tu perfil de Instagram que deseas analizar
 SCROLL_PAUSE_TIME = 2.5 # Pausa entre scrolls (en segundos) para que no se detecte como un bot
 MAX_EXTRA_SCROLLS = 2  # Scrolls adicionales después del final
 MAX_THREADS = 4 # Número máximo de hilos para procesar en paralelo
