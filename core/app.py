d77a86d5541afa755e4610de7c955f2f198ae570
Pablo Delgado <pabloj.df@gmail.com>
2026-04-16 15:46:52 +0200

    Se añade histórico de followers y comparativa con registros anteriores

--- core/app.py
+++ core/app.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from urllib.parse import urlparse
 from time import sleep
-import re
+from datetime import datetime
 import os
 from pathlib import Path
 import pandas as pd
@@ -91,23 +91,39 @@ class InstagramApp:
         ##Esta función obtiene el listado completo de las cuentas que te siguen
         print("Vamos a obtener tus seguidores")
 
+        #Comprobamos si el fichero de histórico existe para crearlo si no. Creamos followers
+        self.excel.controlFollowxExist(historicFollowers)
+        followers = []
+
+        #Comprobamos si estamos en la página del perfil para chequear followers, si no, vamos allí
         if self.driver.current_url != PROFILE_INSTAGRAM:
             print(f"Nos dirigimos a la URL de tu perfil: {PROFILE_INSTAGRAM}")
             self.driver.get(PROFILE_INSTAGRAM)
 
-        followers = []
+        #Recogemos el nº de followers actual
         spanFollowers = self.driver.find_element(By.XPATH, "//span[contains(text(), 'seguidores')]/span[@title]")
         numFollowers = int(spanFollowers.get_attribute("title").replace(",", "").replace(".", ""))
 
-        print (f"Tienes actualmente un total de {numFollowers} seguidores. ")
+        lastNumberFollower, lastDateCheck = self.excel.checkLastNumberFollowx(historicFollowers,numFollowers)
+
+        print (f"\nTienes actualmente un total de {numFollowers} seguidores.")
+
+        if lastNumberFollower == 0:
+            print("\nNo había registros previos")
+        elif lastNumberFollower < numFollowers:
+            print(f"\n¡Bien! Te ha/n seguido ({numFollowers - lastNumberFollower}) cuenta/s nueva/s.")
+        elif lastNumberFollower > numFollowers:
+            print(f"\nVaya, has perdido ({lastNumberFollower - numFollowers}) seguidor/es.")
+        else:
+            print(f"\nTienes los mismos seguidores que la última comprobación, hecha el {lastDateCheck}")
+
 
         self.driver.find_element(By.XPATH, "//*[contains(text(), 'seguidores')]").click()
         self.full_scroll_followX()
 
         listFollowers = self.driver.find_elements(By.CSS_SELECTOR, 'div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x2lah0s.x193iq5w.xeuugli.x1iyjqo2')
         
-        for idx, follower in enumerate(listFollowers, start=1):
-            # Asumiendo que 'elemento' es cada bloque de usuario en la lista
+        for idx, follower in enumerate(listFollowers, start=1):            
             userName = follower.find_element(By.CSS_SELECTOR, "a[href]").get_attribute("href").strip("/").split("/")[-1]
             nombreReal = follower.find_element(By.CSS_SELECTOR, "span.x1lliihq.x193iq5w").text
             link = follower.find_element(By.CSS_SELECTOR, "a[href]").get_attribute("href")
@@ -122,8 +138,6 @@ class InstagramApp:
                 estadoReciproco = "Si"
 
             followers.append([userName, nombreReal, link, estadoReciproco])
-
-            print(f"\n{userName} {nombreReal} {link} {estadoReciproco}")
         
         if followers:
             columnas = ["Usuario", "Nombre", "Link", "Lo sigo"]
--- utils/excel.py
+++ utils/excel.py
@@ -1,6 +1,7 @@
 from config import *
 from datetime import datetime
 import os
+import shutil
 import pandas as pd
 
 class ExcelUtils:
@@ -11,12 +12,53 @@ class ExcelUtils:
         self.base_path = app.base_path
 
 
-    def creaExcel(self, fichero, columnas, ficheroOld):
+    def controlFollowxExist(self, fichero):
+        if not os.path.exists(fichero):
+            print("El fichero de control de followers no existe, lo creo")
+            df = pd.DataFrame(columns=["Número seguidores", "Fecha comprobación"])
+            df.to_excel(fichero, index=False)
 
+    def checkLastNumberFollowx(self, fichero, numFollowers):
+        #Abrimos la excel de histórico y mantenemos en un dataframe
+        dfCheckFollowers = pd.read_excel(historicFollowers)
+        dateActual = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+        #Comprobamos si está vacío para añadir directamente el valor actual, si no, leemos último registro
+        if dfCheckFollowers.empty:
+            #No hay registros
+            with pd.ExcelWriter(historicFollowers, mode='a', if_sheet_exists='overlay') as writer:
+                nuevoRegistro = pd.DataFrame([[numFollowers, dateActual]], columns=["Número seguidores", "Fecha comprobación"])
+                nuevoRegistro.to_excel(writer, index=False, header=False, startrow=writer.sheets['Sheet1'].max_row)
+            lastNumberFollower = 0
+
+        else:
+            lastNumberFollower = dfCheckFollowers.iloc[-1]["Número seguidores"]
+            #Comparamos seguidores actuales con histórico
+            if lastNumberFollower == numFollowers:
+                #Son iguales, solo actualizamos fecha
+                dfCheckFollowers.iloc[-1, dfCheckFollowers.columns.get_loc("Fecha comprobación")] = dateActual
+                dfCheckFollowers.to_excel(historicFollowers, index=False)
+            else:
+                #Son distintos, añadimos nueva fila
+                with pd.ExcelWriter(historicFollowers, mode='a', if_sheet_exists='overlay') as writer:
+                    nuevoRegistro = pd.DataFrame([[numFollowers, dateActual]], columns=["Número seguidores", "Fecha comprobación"])
+                    nuevoRegistro.to_excel(writer, index=False, header=False, startrow=writer.sheets['Sheet1'].max_row)
+            
+        return lastNumberFollower, dateActual
+
+
+
+    def creaExcel(self, fichero, columnas, ficheroOld):
         if os.path.exists(ficheroOld):
             timestamp = datetime.now().strftime("%Y%m%d_%H%M")
             newName = str(ficheroOld).replace(".xlsx", f"-{timestamp}.xlsx")
             os.rename(ficheroOld, newName)
+            
+            if not os.path.exists(folderOld):
+                os.mkdir(folderOld)
+            
+            #Movemos los históricos a una carpeta concreta
+            shutil.move(newName, folderOld)
 
         if os.path.exists(fichero):
             os.rename(fichero, ficheroOld)
